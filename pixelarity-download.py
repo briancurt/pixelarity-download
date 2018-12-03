3053aaddc6e6e1b13dc7503a09dea19fbbe67d39
briancurt <bcurtich@gmail.com>
2018-12-03 11:26:40 -0300

    Reformat

--- pixelarity-download.py
+++ pixelarity-download.py
@@ -8,39 +8,71 @@ from bs4 import BeautifulSoup
 from zipfile import ZipFile
 from shutil import copyfileobj
 
+
 def download_template(template, template_type, force_download, browser):
     """
     Download a Pixelarity template in a given format, or retry to download it Pixelarity returns
     a HTTP 429 message.
     """
-    print("[*] Downloading https://pixelarity.com/" + template + "/download/" + template_type + " ...")
+    print(
+        "[*] Downloading https://pixelarity.com/"
+        + template
+        + "/download/"
+        + template_type
+        + " ..."
+    )
     if not path.exists("./px-" + template + "/"):
         makedirs("./px-" + template + "/")
-    if not path.exists("./px-" + template + "/px-" + template + "-" + template_type + ".zip") or force_download:
-        request = browser.session.get("https://pixelarity.com/" + template + "/download/" + template_type, stream=True)
+    if (
+        not path.exists(
+            "./px-" + template + "/px-" + template + "-" + template_type + ".zip"
+        )
+        or force_download
+    ):
+        request = browser.session.get(
+            "https://pixelarity.com/" + template + "/download/" + template_type,
+            stream=True,
+        )
         if request.status_code == 200:
-            with open("./px-" + template + "/px-" + template + "-" + template_type + ".zip", "wb") as temp_zip:
+            with open(
+                "./px-" + template + "/px-" + template + "-" + template_type + ".zip",
+                "wb",
+            ) as temp_zip:
                 request.raw.decode_content = True
                 copyfileobj(request.raw, temp_zip)
         elif request.status_code == 429:
-            print("[*] Server returned HTTP 429 (Too Many Requests), attempting to re-download template " + template + " after 60s timeout")
+            print(
+                "[*] Server returned HTTP 429 (Too Many Requests), attempting to re-download template "
+                + template
+                + " after 60s timeout"
+            )
             sleep(60)
             download_template(template, template_type, force_download, browser)
         else:
             print("[!] Could not download theme " + template)
     else:
-        print("[!] Template " + template + "is already downloaded and option --force is not specified, skipping...")
+        print(
+            "[!] Template "
+            + template
+            + "is already downloaded and option --force is not specified, skipping..."
+        )
     print("[+] Template " + template + " successfully downloaded")
     print()
+
 
 def extract_template(template, template_type, force_download):
     """
     Extract the given template zipfile unless the file already exists or option --force
     is set when calling the program.
     """
-    if not path.exists("./px-" + template + "/" + template_type + "/") or force_download:
+    if (
+        not path.exists("./px-" + template + "/" + template_type + "/")
+        or force_download
+    ):
         makedirs("./px-" + template + "/" + template_type + "/")
-        zip_ref = ZipFile("./px-" + template + "/px-" + template + "-" + template_type + ".zip", "r")
+        zip_ref = ZipFile(
+            "./px-" + template + "/px-" + template + "-" + template_type + ".zip", "r"
+        )
         zip_ref.extractall("./px-" + template + "/" + template_type + "/")
         zip_ref.close()
 
@@ -62,35 +94,55 @@ def get_templates():
 
     return templates
 
+
 def main():
 
-        parser = ArgumentParser(description="Pixelarity template downloader")
-        parser.add_argument("email")
-        parser.add_argument("password")
-        parser.add_argument('-f', '--force', help="Force redownload of all themes. Supply `True` or `False`.", action='store_true', required=False)
-        parser.add_argument('-p', '--psd', help="Download PSD themes too.", action='store_true', required=False)
-        parser.add_argument('-x', '--extract', help="Extract templates zipfiles.", action='store_true', required=False)
-        args = parser.parse_args()
+    parser = ArgumentParser(description="Pixelarity template downloader")
+    parser.add_argument("email")
+    parser.add_argument("password")
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="Force redownload of all themes. Supply `True` or `False`.",
+        action="store_true",
+        required=False,
+    )
+    parser.add_argument(
+        "-p",
+        "--psd",
+        help="Download PSD themes too.",
+        action="store_true",
+        required=False,
+    )
+    parser.add_argument(
+        "-x",
+        "--extract",
+        help="Extract templates zipfiles.",
+        action="store_true",
+        required=False,
+    )
+    args = parser.parse_args()
 
-        browser = RoboBrowser()
-        templates = get_templates()
+    browser = RoboBrowser()
+    templates = get_templates()
 
-        browser.open("https://pixelarity.com/login")
+    browser.open("https://pixelarity.com/login")
 
-        login_form = browser.get_form(id="ajaxForm1")
+    login_form = browser.get_form(id="ajaxForm1")
 
-        login_form["email"].value = args.email
-        login_form["password"].value = args.password
+    login_form["email"].value = args.email
+    login_form["password"].value = args.password
 
-        browser.submit_form(login_form)
+    browser.submit_form(login_form)
 
-        for template in templates:
-            for template_type in ["html", "psd"]:
-                download_template(template, template_type, args.force, browser)
-                if args.extract:
-                    extract_template(template, template_type, args.force)
+    for template in templates:
+        for template_type in ["html", "psd"]:
+            download_template(template, template_type, args.force, browser)
+            if args.extract:
+                extract_template(template, template_type, args.force)
 
-        print("[+] All themes downloaded successfully!")
+    print("[+] All themes downloaded successfully!")
+
 
 if __name__ == "__main__":
     main()
